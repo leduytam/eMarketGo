64d3757a88da946661d0d4af74413b9e60f340f5
btsang <105647814+btsangk20@users.noreply.github.com>
2023-04-17 01:00:12 +0700

    EG03 | profile screen, all history screen and order detail screen (#3)

--- app/src/main/java/com/group05/emarketgo/views/fragments/HomeFragment.java
+++ app/src/main/java/com/group05/emarketgo/views/fragments/HomeFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +17,8 @@ public class HomeFragment extends Fragment {
 
     private Context context;
     private static FirebaseAuth mAuth;
+
+    private LinearLayout orderDetailLayout;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -33,6 +36,19 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         context = getContext();
 
+        orderDetailLayout = view.findViewById(R.id.order_detail);
+        orderDetailLayout.setOnClickListener(v -> onClickOrderDetail());
+
         return view;
+    }
+
+    private void onClickOrderDetail() {
+        int fragmentId = this.getId();
+        OrderDetailFragment orderDetailFragment = new OrderDetailFragment();
+        requireActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .replace(fragmentId, orderDetailFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
